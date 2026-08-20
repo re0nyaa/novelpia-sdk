e675d6628a6f5ec18416982342c2f36b310b7bbc
Minjae Ryu <re.0.nyaa0@gmail.com>
2026-08-20 23:26:35 +0900

    chore: dist제거

--- dist/index.js
+++
@@ -1,7 +0,0 @@
-import { NovelPiaClient } from "./src/client";
-export * from "./src/types";
-export * from "./src/errors";
-export * from "./src/cache";
-export * from "./src/retry";
-export * from "./src/client";
-export default NovelPiaClient;
